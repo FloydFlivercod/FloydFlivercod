daebd98835d7b7e39094551ca73f4b6878b35f56
Ike Gilbert <email2ike@gmail.com>
2016-01-26 12:50:58 -0500

    Update floydflivercod-aicp.sh

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -2,20 +2,20 @@ mkdir flivercod-aicp
 cd flivercod-aicp
 repo init -u https://github.com/AICP/platform_manifest.git -b mm6.0
 repo sync --force-sync
-rm -rf flivercod-aicp/FloydFlivercod
+rm -rf FloydFlivercod
 git clone https://github.com/IkeGilbert/FloydFlivercod.git
 cd ~/flivercod-aicp/flivercod-aicp/FloydFlivercod
 head -25 README1.md
 cd ~/flivercod-aicp
 . build/envsetup.sh
-rm -rf flivercod-aicp/.repo/local_manifests
 cd ~/flivercod-aicp/.repo
+rm -rf local_manifests
 git clone https://github.com/IkeGilbert/local_manifest.git
 cd ~/flivercod-aicp/vendor
-rm -rf flivercod-aicp/vendor/aicp
+rm -rf aicp
 cd ~/flivercod-aicp/packages/apps
-rm -rf flivercod-aicp/packages/apps/Settings
-rm -rf flivercod-aicp/packages/apps/AicpExtras
+rm -rf Settings
+rm -rf AicpExtras
 cd ~/flivercod-aicp/vendor
 git clone https://github.com/IkeGilbert/aicp.git
 cd ~/flivercod-aicp/packages/apps
