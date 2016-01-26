7612c1286652d47230e90b474f9241cef94cec2c
Ike Gilbert <email2ike@gmail.com>
2016-01-26 13:01:32 -0500

    Update floydflivercod-aicp.sh

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -18,9 +18,12 @@ rm -rf Settings
 rm -rf AicpExtras
 cd ~/flivercod-aicp/vendor
 git clone https://github.com/IkeGilbert/aicp.git
-cd ~/flivercod-aicp/packages/apps
+cd ~/flivercod-aicp/packages
 git clone https://github.com/IkeGilbert/Settings.git
+mv ~/flivercod-aicp/packages/Settings ~/flivercod-aicp/packages/apps
+cd ~/flivercod-aicp/packages
 git clone https://github.com/IkeGilbert/AicpExtras.git
+mv ~/flivercod-aicp/packages/AicpExtras ~/flivercod-aicp/packages/apps
 cd ~/flivercod-aicp/flivercod-aicp/FloydFlivercod
 head -25 README2.md
 cd ~/flivercod-aicp
