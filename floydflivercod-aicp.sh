553d4419bd771b4fc7cd4134de2838cf11d6728b
Ike Gilbert <email2ike@gmail.com>
2016-02-13 13:56:57 -0500

    Update floydflivercod-aicp.sh

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -42,5 +42,6 @@ mv ~/flivercod-aicp/packages/AICPapers ~/flivercod-aicp/packages/wallpapers
 cd ~/flivercod-aicp/FloydFlivercod
 head -25 README2.md
 cd ~/flivercod-aicp
-
+sudo mkdir ~/out
+export OUT_DIR_COMMON_BASE=/home/ikeg/flivercod-aicp/out
 
