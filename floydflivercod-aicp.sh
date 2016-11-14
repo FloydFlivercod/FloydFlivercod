2809527f55ecc6642b0dabf760f7c1b553aa3bf1
Ike Gilbert <email2ike@gmail.com>
2016-11-13 23:42:05 -0500

    Update floydflivercod-aicp.sh

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -1,3 +1,10 @@
+cd ~/AICP/vendor
+mv ~/AICP/vendor/aicp ~/AICP/vendor/aicp1
+cd ~/AICP/packages/apps
+mv ~/AICP/packages/apps/AicpExtras ~/AICP/packages/apps/AicpExtras1
+mv ~/AICP/packages/apps/Settings ~/AICP/packages/apps/Settings1
+repo init -u https://github.com/AICP/platform_manifest.git -b n7.1
+repo sync --force-sync && . build/envsetup.sh
 rm -rf FloydFlivercod
 cd ~/AICP
 git clone https://github.com/IkeGilbert/FloydFlivercod.git
@@ -6,25 +13,16 @@ head -25 README.md
 cd ~/AICP/packages/apps
 rm -rf Settings
 rm -rf AicpExtras
-cd ~/AICP
-. build/envsetup.sh
 cd ~/AICP/vendor
 rm -rf aicp
-git clone https://github.com/IkeGilbert/aicp.git
+mv ~/AICP/vendor/aicp1 ~/AICP/vendor/aicp
 cd ~/AICP/packages/apps
-rm -rf Settings
-rm -rf AicpExtras
-rm -rf CustomWallpapers
-cd ~/AICP/packages
-git clone https://github.com/IkeGilbert/Settings.git
-mv ~/AICP/packages/Settings ~/AICP/packages/apps
-cd ~/AICP/packages
-git clone https://github.com/IkeGilbert/AicpExtras.git
-mv ~/AICP/packages/AicpExtras ~/AICP/packages/apps
-cd ~/AICP/packages
+mv ~/AICP/packages/apps/AicpExtras1 ~/AICP/packages/apps/AicpExtras
+mv ~/AICP/packages/apps/Settings1 ~/AICP/packages/apps/Settings
 git clone https://github.com/IkeGilbert/CustomWallpapers.git
 mv ~/AICP/packages/CustomWallpapers ~/AICP/packages/apps/CustomWallpapers
-cd ~/AICP
 cd ~/AICP/FloydFlivercod
 head -25 README2.md
 cd ~/AICP
+source build/envsetup.sh
+brunch bacon
