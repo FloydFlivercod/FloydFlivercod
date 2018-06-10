413654c59b31fdaa7decfffec1ea3fb5c7aa9bba
FlivercodROM <FloydFlivercod@gmail.com>
2018-06-10 13:13:55 -0400

    Initial purge to O

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -3,7 +3,9 @@ mv ~/FlivercodROM/vendor/aicp ~/FlivercodROM/vendor/aicp1
 cd ~/FlivercodROM/packages/apps
 mv ~/FlivercodROM/packages/apps/AicpExtras ~/FlivercodROM/packages/apps/AicpExtras1
 mv ~/FlivercodROM/packages/apps/Settings ~/FlivercodROM/packages/apps/Settings1
-repo init -u https://github.com/FloydFlivercod/platform_manifest.git -b o8.0
+cd ~/FlivercodROM/device
+mv ~/FlivercodROM/device/oneplus ~/FlivercodROM/device/oneplus1
+repo init -u https://github.com/AICP/platform_manifest.git -b o8.1
 repo sync --force-sync && . build/envsetup.sh
 rm -rf FloydFlivercod
 cd ~/FlivercodROM
@@ -17,6 +19,9 @@ cd ~/FlivercodROM/vendor
 rm -rf aicp
 git clone https://github.com/FloydFlivercod/vendor_aicp.git
 mv ~/FlivercodROM/vendor/vendor_aicp ~/FlivercodROM/vendor/aicp
+cd ~/FlivercodROM/device
+git clone https://github.com/FloydFlivercod/device_oneplus_oneplus3.git
+mv ~/FlivercodROM/device/oneplus/device_oneplus_oneplus3 ~/FlivercodROM/device/oneplus/oneplus3
 cd ~/FlivercodROM/packages/apps
 mv ~/FlivercodROM/packages/apps/AicpExtras1 ~/FlivercodROM/packages/apps/AicpExtras
 mv ~/FlivercodROM/packages/apps/Settings1 ~/FlivercodROM/packages/apps/Settings
