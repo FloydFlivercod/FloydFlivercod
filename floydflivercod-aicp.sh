adf264f564aa7f8bee548e0095c089dbc4c428af
FlivercodROM <FloydFlivercod@gmail.com>
2018-11-03 12:04:04 -0400

    update for PIE

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -1,29 +1,32 @@
-cd ~/FlivercodROM/vendor
-mv ~/FlivercodROM/vendor/aicp ~/FlivercodROM/vendor/aicp1
-cd ~/FlivercodROM/packages/apps
-mv ~/FlivercodROM/packages/apps/AicpExtras ~/FlivercodROM/packages/apps/AicpExtras1
-mv ~/FlivercodROM/packages/apps/Settings ~/FlivercodROM/packages/apps/Settings1
-cd ~/FlivercodROM/device
-mv ~/FlivercodROM/device/oneplus ~/FlivercodROM/device/oneplus1
+cd ~/aicpP/vendor
+mv ~/aicpP/vendor/aicp ~/aicpP/vendor/aicp1
+cd ~/aicpP/packages/apps
+mv ~/aicpP/packages/apps/AicpExtras ~/aicpP/packages/apps/AicpExtras1
+cd ~/aicpP/device
+mkdir ~/aicpP/device/oneplus
+mv ~/aicpP/device/oneplus ~/aicpP/device/oneplus1
+mv ~/aicpP/device/oppo/common ~/aicpP/device/oppo/common1
 repo init -u https://github.com/AICP/platform_manifest.git -b o8.1
 repo sync --force-sync && . build/envsetup.sh
 rm -rf FloydFlivercod
-cd ~/FlivercodROM
-git clone https://github.com/IkeGilbert/FloydFlivercod.git
-cd ~/FlivercodROM/FloydFlivercod
+cd ~/aicpP
+git clone https://github.com/FloydFlivercod/FloydFlivercod.git
+cd ~/aicpP/FloydFlivercod
 head -25 README.md
-cd ~/FlivercodROM/packages/apps
-rm -rf Settings
+cd ~/aicpP/packages/apps
 rm -rf AicpExtras
-cd ~/FlivercodROM/vendor
+cd ~/aicpP/vendor
 rm -rf aicp
 git clone https://github.com/FloydFlivercod/aicp.git
-cd ~/FlivercodROM/device
-https://github.com/FloydFlivercod/oneplus3.git
-cd ~/FlivercodROM/packages/apps
+mkdir aicpP/device/oneplus
+cd ~/aicpP/device/oneplus
+git clone https://github.com/FloydFlivercod/oneplus3.git
+cd ~/aicpP/device/oppo
+git clone https://github.com/FloydFlivercod/common.git
+cd ~/aicpP/packages/apps
 git clone https://github.com/FloydFlivercod/AicpExtras.git
-cd ~/FlivercodROM/FloydFlivercod
+cd ~/aicpP/FloydFlivercod
 head -25 README2.md
-cd ~/FlivercodROM
+cd ~/aicpP
 source build/envsetup.sh
 brunch oneplus3
