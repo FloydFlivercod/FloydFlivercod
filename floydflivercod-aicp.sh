8454dc5f40a4b0094014527daf9d39308a56ccf0
FlivercodROM <FloydFlivercod@gmail.com>
2018-11-03 23:35:57 -0400

    operations
    changing sequence

--- floydflivercod-aicp.sh
+++ floydflivercod-aicp.sh
@@ -1,32 +1,28 @@
-cd ~/aicpP/vendor
-mv ~/aicpP/vendor/aicp ~/aicpP/vendor/aicp1
-cd ~/aicpP/packages/apps
-mv ~/aicpP/packages/apps/AicpExtras ~/aicpP/packages/apps/AicpExtras1
-cd ~/aicpP/device
+rm -rf ~/aicpP/device/oneplus/oneplus3/
 mkdir ~/aicpP/device/oneplus
-mv ~/aicpP/device/oneplus ~/aicpP/device/oneplus1
-mv ~/aicpP/device/oppo/common ~/aicpP/device/oppo/common1
-repo init -u https://github.com/AICP/platform_manifest.git -b o8.1
-repo sync --force-sync && . build/envsetup.sh
-rm -rf FloydFlivercod
-cd ~/aicpP
+rm -rf ~/aicpP/FloydFlivercod
+rm -rf ~/aicpP/vendor/aicp
+rm -rf ~/aicpP/device/oppo/common
+rm -rf ~/aicpP/packages/apps/AicpExtras
+mkdir ~/aicpP/FloydFlivercod
 git clone https://github.com/FloydFlivercod/FloydFlivercod.git
 cd ~/aicpP/FloydFlivercod
 head -25 README.md
-cd ~/aicpP/packages/apps
-rm -rf AicpExtras
-cd ~/aicpP/vendor
-rm -rf aicp
+repo init -u https://github.com/AICP/platform_manifest.git -b p9.0
+repo sync --force-sync && . build/envsetup.sh
+rm -rf ~/aicpP/packages/apps/AicpExtras
+rm -rf ~/aicpP/vendor/aicp
 git clone https://github.com/FloydFlivercod/aicp.git
+cd ~/aicpP/device
 mkdir aicpP/device/oneplus
 cd ~/aicpP/device/oneplus
 git clone https://github.com/FloydFlivercod/oneplus3.git
 cd ~/aicpP/device/oppo
+rm -rf ~/aicpP/device/oppo/common
 git clone https://github.com/FloydFlivercod/common.git
 cd ~/aicpP/packages/apps
+rm -rf ~/aicpP/packages/apps/AicpExtras
 git clone https://github.com/FloydFlivercod/AicpExtras.git
 cd ~/aicpP/FloydFlivercod
 head -25 README2.md
 cd ~/aicpP
-source build/envsetup.sh
-brunch oneplus3
